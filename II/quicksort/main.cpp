86fddd5dc4c0cc3431fa2c578e4057de6e63406a
Huguenin-Elie Steve <steve.huguenin-elie@he-arc.ch>
2022-02-21 13:50:58 +0000

    quicksort

--- II/quicksort/main.cpp
+++ II/quicksort/main.cpp
@@ -7,14 +7,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "allocation()" << endl;
-    int64_t values[] = {26, 28, 5, 25, 20, 35, 60, 75, 12, 42, 23};
-    shared_ptr<int64_t[]> tab = make_shared<int64_t[]>(11);
-    cout << "copie()" << endl;
+    shared_ptr<int64_t[]> tab(new int64_t[11]{26, 28, 5, 25, 20, 35, 60, 75, 12, 42, 23});
+    cout << "affichage()" << endl;
     cout << "[ ";
     for (int i = 0; i < 11; ++i)
     {
-        tab[i] = values[i];
-        cout << setw(2) << values[i] << " ";
+        cout << setw(2) << tab[i] << " ";
     }
     cout << "]" << endl;
     cout << "explorer()" << endl;
